ba10a5af8b144967bdc449d9fc4b69c712046cdd
Jeff Heaton <jeff@jeffheaton.com>
2013-09-03 07:42:40 -0500

    code cleanup

--- vol1/java-examples/src/main/java/com/heatonresearch/aifh/error/package-info.java
+++ vol1/java-examples/src/main/java/com/heatonresearch/aifh/error/package-info.java
@@ -26,19 +26,4 @@
  * and trademarks visit:
  * http://www.heatonresearch.com/copyright
  */
-package com.heatonresearch.aifh.distance;
-
-/**
- * Created with IntelliJ IDEA.
- * User: jheaton
- * Date: 7/25/13
- * Time: 4:45 AM
- * To change this template use File | Settings | File Templates.
- */
-public abstract class AbstractDistance implements CalculateDistance {
-
-    @Override
-    public double calculate(double[] position1, double[] position2) {
-        return calculate(position1, 0, position2, 0, position1.length);
-    }
-}
+package com.heatonresearch.aifh.error;
--- vol1/java-examples/src/main/java/com/heatonresearch/aifh/examples/distance/Sample.java
+++ vol1/java-examples/src/main/java/com/heatonresearch/aifh/examples/distance/Sample.java
@@ -1,9 +1,14 @@
 /*
- * Encog(tm) Java Examples v3.2
- * http://www.heatonresearch.com/encog/
- * https://github.com/encog/encog-java-examples
+ * Artificial Intelligence for Humans
+ * Volume 1: Fundamental Algorithms
+ * Java Version
+ * http://www.aifh.org
+ * http://www.jeffheaton.com
  *
- * Copyright 2008-2013 Heaton Research, Inc.
+ * Code repository:
+ * https://github.com/jeffheaton/aifh
+
+ * Copyright 2013 by Jeff Heaton
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,8 +21,8 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *   
- * For more information on Heaton Research copyrights, licenses 
+ *
+ * For more information on Heaton Research copyrights, licenses
  * and trademarks visit:
  * http://www.heatonresearch.com/copyright
  */
