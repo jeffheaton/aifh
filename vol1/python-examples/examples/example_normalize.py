8156e50fcc3c5bd19beb3d6296192f444b93586b
Jeff Heaton <jeff@jeffheaton.com>
2013-11-23 07:52:58 -0600

    Some reorg of Python structure, and added normalize unit test

--- vol1/python-examples/examples/example_normalize.py
+++ vol1/python-examples/examples/example_normalize.py
@@ -1,79 +1,16 @@
 __author__ = 'jheaton'
 
 import os
-import csv
 import sys
 
+# Import for the library that we're testing here
+distance_dir = os.path.dirname(__file__) + os.sep + ".." + \
+  os.sep + "lib" + os.sep + "aifh"
+sys.path.append(distance_dir)
+print(os.path.abspath(distance_dir))
 
-def load_csv(filename):
-    result = []
-    first = True
+from normalize import Normalize
 
-    with open(filename, 'rt') as f:
-        reader = csv.reader(f)
-        for row in reader:
-            if len(row) > 0:
-                if first:
-                    first = False
-                    header = row
-                else:
-                    result.append(row)
-    return result
-
-
-def displayData(dataset):
-    for row in dataset:
-        print(row)
-
-
-def normMax(dataset, col):
-    result = sys.float_info.min
-    for row in dataset:
-        result = max(result, row[col])
-    return result
-
-
-def normMin(dataset, col):
-    result = sys.float_info.max
-    for row in dataset:
-        result = min(result, row[col])
-    return result
-
-
-def normMakeNumeric(dataset, col):
-    for row in dataset:
-        row[col] = float(row[col])
-
-
-def normRange(dataset, col, normalizedLow, normalizedHigh):
-    dataLow = normMin(dataset, col)
-    dataHigh = normMax(dataset, col)
-
-    for row in dataset:
-        row[col] = ((row[col] - dataLow) / (dataHigh - dataLow)) * (normalizedHigh - normalizedLow) + normalizedLow
-
-
-def buildClassMap(dataset, col):
-    result = {}
-    index = 0
-    for row in dataset:
-        key = row[col]
-        if key not in result:
-            result[key] = index
-            index += 1
-    return result
-
-
-def normOneOfN(dataset, col, classes):
-    for row in dataset:
-        key = row[col]
-        value = classes[key]
-        row.pop(col)
-        for i in range(0, len(classes)):
-            if i == value:
-                row.insert(col + i, 1)
-            else:
-                row.insert(col + 1, 0)
 
 # find the Iris data set
 irisFile = os.path.dirname(os.path.realpath(__file__))
@@ -81,14 +18,16 @@ irisFile = os.path.abspath(irisFile + "../../datasets/iris.csv")
 
 print('Reading CSV file: ' + irisFile)
 
-result = loadCSV(irisFile)
-for i in range(0, 4):
-    normMakeNumeric(result, i)
-    normRange(result, i, -1, 1)
+norm = Normalize()
 
-classes = buildClassMap(result, 4)
-normOneOfN(result, 4, classes)
-displayData(result)
+result = norm.load_csv(irisFile)
+for i in range(0, 4):
+    norm.make_numeric(result, i)
+    norm.norm_range(result, i, -1, 1)
+
+classes = norm.build_class_map(result, 4)
+norm.norm_one_of_n(result, 4, classes)
+norm.display_data(result)
 
 print(classes)
 
---
+++ vol1/python-examples/test/aifh/test_normalize.py
@@ -0,0 +1,58 @@
+import unittest
+import os
+import sys
+
+__author__ = 'jheaton'
+
+
+# Import for the library that we're testing here
+aifh_dir = os.path.dirname(__file__) + os.sep + ".." + \
+  os.sep + ".." + os.sep + "lib" + os.sep + "aifh"
+sys.path.append(aifh_dir)
+
+from normalize import Normalize
+from aifh_error import AIFHError
+
+class TestNormalize(unittest.TestCase):
+
+
+    def test_normalize(self):
+        # find the Iris data set
+        irisFile = os.path.dirname(os.path.realpath(__file__))
+        irisFile = os.path.abspath(irisFile + "../../../datasets/iris.csv")
+
+        norm = Normalize()
+
+        result = norm.load_csv(irisFile)
+
+        self.assertEqual(len(norm.column_map),5)
+        self.assertEqual(len(norm.header),5)
+        self.assertEqual(norm.header[0],"sepal_length")
+        self.assertEqual(norm.header[1],"sepal_width")
+        self.assertEqual(norm.header[2],"petal_length")
+        self.assertEqual(norm.header[3],"petal_width")
+        self.assertEqual(norm.header[4],"class")
+        self.assertTrue("sepal_length" in norm.column_map)
+        self.assertTrue("sepal_width" in norm.column_map)
+        self.assertTrue("petal_length" in norm.column_map)
+        self.assertTrue("petal_width" in norm.column_map)
+        self.assertTrue("class" in norm.column_map)
+        self.assertEqual(norm.resolve_column("sepal_length"),0)
+        self.assertEqual(norm.resolve_column("sepal_width"),1)
+        self.assertEqual(norm.resolve_column("petal_length"),2)
+        self.assertEqual(norm.resolve_column("petal_width"),3)
+        self.assertEqual(norm.resolve_column("class"),4)
+        self.assertRaises(AIFHError, norm.resolve_column, 6)
+        self.assertRaises(AIFHError, norm.resolve_column, "unknown")
+
+        for i in range(0, 4):
+            norm.make_numeric(result, i)
+            norm.norm_range(result, i, -1, 1)
+
+        self.assertAlmostEqual(result[0][0],-0.555,2)
+        self.assertAlmostEqual(result[0][1], 0.249,2)
+        self.assertAlmostEqual(result[0][2],-0.864,2)
+        self.assertAlmostEqual(result[0][3],-0.916,2)
+
+        classes = norm.build_class_map(result, 4)
+        norm.norm_one_of_n(result, 4, classes)
