a615660ba469755e6d6f3ef92a4c3fd1772f4067
Jeff Heaton <jeff@jeffheaton.com>
2013-11-24 18:38:38 -0600

    Removed distance calculate classes, using SciPy for that.

--- vol1/python-examples/examples/example_distance.py
+++ vol1/python-examples/examples/example_distance.py
@@ -1,36 +1,55 @@
+"""
+    Artificial Intelligence for Humans
+    Volume 1: Fundamental Algorithms
+    Python Version
+    http://www.aifh.org
+    http://www.jeffheaton.com
+
+    Code repository:
+    https://github.com/jeffheaton/aifh
+
+    Copyright 2013 by Jeff Heaton
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+
+    For more information on Heaton Research copyrights, licenses
+    and trademarks visit:
+    http://www.heatonresearch.com/copyright
+"""
 __author__ = 'jheaton'
 
 import os
 import sys
+from scipy.spatial import distance
 
 # Find the AIFH core files
 aifh_dir = os.path.dirname(os.path.abspath(__file__))
 aifh_dir = os.path.abspath(aifh_dir + os.sep + ".." + os.sep + "lib" + os.sep + "aifh")
 sys.path.append(aifh_dir)
 
-
-from euclidean_distance import EuclideanDistance
-from manhattan_distance import ManhattanDistance
-from chebyshev_distance import ChebyshevDistance
-
-
 pos1 = [ 1.0, 2.0, 3.0 ]
 pos2 = [ 4.0, 5.0, 6.0 ]
 pos3 = [ 7.0, 8.0, 9.0 ]
 
-calcE = EuclideanDistance()
-calcM = ManhattanDistance()
-calcC = ChebyshevDistance()
-
 print("Euclidean Distance")
-print("pos1->pos2: ", calcE.calculate(pos1,pos2))
-print("pos2->pos3: ", calcE.calculate(pos2,pos3))
-print("pos3->pos1: ", calcE.calculate(pos3,pos1))
-print("\nManhattan Distance\n")
-print("pos1->pos2: ", calcM.calculate(pos1,pos2))
-print("pos2->pos3: ", calcM.calculate(pos2,pos3))
-print("pos3->pos1: ", calcM.calculate(pos3,pos1))
+print("pos1->pos2: " + str(distance.euclidean(pos1,pos2)))
+print("pos2->pos3: " + str(distance.euclidean(pos2,pos3)))
+print("pos3->pos1: " + str(distance.euclidean(pos3,pos1)))
+print("\nManhattan (city block) Distance\n")
+print("pos1->pos2: " + str(distance.cityblock(pos1,pos2)))
+print("pos2->pos3: " + str(distance.cityblock(pos2,pos3)))
+print("pos3->pos1: " + str(distance.cityblock(pos3,pos1)))
 print("\nChebyshev Distance\n")
-print("pos1->pos2: ", calcC.calculate(pos1,pos2))
-print("pos2->pos3: ", calcC.calculate(pos2,pos3))
-print("pos3->pos1: ", calcC.calculate(pos3,pos1))
+print("pos1->pos2: " + str(distance.chebyshev(pos1,pos2)))
+print("pos2->pos3: " + str(distance.chebyshev(pos2,pos3)))
+print("pos3->pos1: " + str(distance.chebyshev(pos3,pos1)))
--- vol1/python-examples/test/test_suite.py
+++ vol1/python-examples/test/test_suite.py
@@ -8,15 +8,11 @@ import unittest
 
 
 # imports for distance tests
-from aifh.test_chebyshev_distance import TestChebyshevDistance
-from aifh.test_euclidean_distance import TestEuclideanDistance
-from aifh.test_manhattan_distance import TestManhattanDistance
 from aifh.test_normalize import TestNormalize
 from aifh.test_equilateral import TestEquilateral
 
 
-test_cases = [TestChebyshevDistance, TestEuclideanDistance,
-  TestManhattanDistance, TestNormalize, TestEquilateral]
+test_cases = [TestNormalize, TestEquilateral]
 test_suite = unittest.TestSuite()
 for test_class in test_cases:
   tests = unittest.TestLoader().loadTestsFromTestCase(test_class)
