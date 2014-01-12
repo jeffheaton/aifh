55f60300793cf79f18216f07101ff15253abfd77
Jeff Heaton <jeff@jeffheaton.com>
2014-01-12 06:37:47 -0600

    vol2: Setup initial directory structure for vol2 and Java examples

---
+++ vol2/README.md
@@ -0,0 +1,28 @@
+AIFH Vol2: Nature Inspired Algorithms (Under construction!)
+====
+> Note: This book is not yet published and the Volume 2 examples are very much
+> **Under Construction**.
+
+This book has the following chapters.
+
+* Chapter 1: Population and Scoring
+* Chapter 2: Crossover and Mutation
+* Chapter 3: Genetic Algorithms
+* Chapter 4: Modeling Problems
+* Chapter 5: Genetic Programming
+* Chapter 6: Bayesian GP
+* Chapter 7: Speciation
+* Chapter 8: Particle Swarm Optimization
+* Chapter 9: Ant Colony Optimization 
+* Chapter 10: Artificial Life
+
+The following language ports have been completed for this book.  See the individual 
+README files inside of each chapter for language specific instructions. If you find a bug,
+or improve an example, please consider pushing a change via GitHub.
+
+* vol2-csharp-examples	
+* vol2-java-examples
+		
+If you find a language folder, other than those above, the language is likely "under
+construction".  In this case, there should be more information in the language specific
+folder.
---
+++ vol2/vol2-java-examples/src/main/java/com/heatonresearch/aifh/examples/intro/CSVExample.java
@@ -0,0 +1,84 @@
+/*
+ * Artificial Intelligence for Humans
+ * Volume 1: Fundamental Algorithms
+ * Java Version
+ * http://www.aifh.org
+ * http://www.jeffheaton.com
+ *
+ * Code repository:
+ * https://github.com/jeffheaton/aifh
+
+ * Copyright 2013 by Jeff Heaton
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * For more information on Heaton Research copyrights, licenses
+ * and trademarks visit:
+ * http://www.heatonresearch.com/copyright
+ */
+
+package com.heatonresearch.aifh.examples.intro;
+
+import au.com.bytecode.opencsv.CSVReader;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+/**
+ * Shows how to read a CSV file.
+ */
+public class CSVExample {
+
+    /**
+     * Run the example.
+     */
+    public void run() {
+        CSVReader reader = null;
+
+        try {
+            final InputStream istream = this.getClass().getResourceAsStream("/iris.csv");
+            if( istream==null ) {
+                System.out.println("Cannot access data set, make sure the resources are available.");
+                System.exit(1);
+            }
+            reader = new CSVReader(new InputStreamReader(istream));
+
+            String[] nextLine;
+            while ((nextLine = reader.readNext()) != null) {
+                System.out.println(Arrays.toString(nextLine));
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        }
+    }
+
+    /**
+     * The main method.
+     *
+     * @param args Arg 0 is the file to read.
+     */
+    public static void main(final String[] args) {
+        final CSVExample prg = new CSVExample();
+        prg.run();
+    }
+}
