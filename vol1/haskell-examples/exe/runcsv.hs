160644b47bff4d3e16ae3745a797f6eb761fd773
jpmoresmau <jp@moresmau.fr>
2015-10-06 21:53:25 +0100

    simple csv reading

---
+++ vol1/haskell-examples/exe/runcsv.hs
@@ -0,0 +1,22 @@
+-- This example reads a CSV file
+module Main where
+
+import System.Directory
+
+import Data.Csv
+import Data.ByteString.Lazy as BS
+import Data.Vector (Vector)
+
+main :: IO()
+main = do
+   let f="data/iris.csv"
+   ex <- doesFileExist f
+   case ex of
+     False -> print "Input file does not exist"
+     _ -> do
+        bs <- BS.readFile f
+        -- read simply as a tuple
+        let result = decode HasHeader bs :: Either String (Vector (Double,Double,Double,Double,String))
+        case result of
+          Left err  -> print $ "Error reading CSV File:" ++ err
+          Right dat -> mapM_ print dat
