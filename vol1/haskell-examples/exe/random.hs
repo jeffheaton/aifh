1277476a96fec89bebcd14e2637a3c13b5af57d6
jpmoresmau <jp@moresmau.fr>
2015-10-26 21:59:42 +0000

    random distribution

---
+++ vol1/haskell-examples/exe/random.hs
@@ -0,0 +1,45 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+-- | Chapter 4: random numbers
+module Main where
+
+import AI.AIFH.Utils
+
+import Control.Monad
+import Data.List
+import System.Environment
+
+import System.Random.MWC
+import System.Random.MWC.Distributions
+
+import Graphics.Rendering.Chart.Easy
+import Graphics.Rendering.Chart.Backend.Cairo
+
+-- | Entry point
+main :: IO ()
+main = do
+    args <- getArgs
+    case args of
+        ["uniform"] -> generateHistogram Uniform
+        ["normal"]  -> generateHistogram Normal
+        _ -> do
+            n<-getProgName
+            putStrLn $ "usage "++n++" uniform|normal"
+
+-- | Type of distribution
+data Distribution = Uniform | Normal
+    deriving (Show,Read,Eq,Ord,Bounded,Enum)
+
+-- | Generate the frequency histogram to a png file
+generateHistogram :: Distribution -> IO ()
+generateHistogram d = do
+    -- random values
+    vals::[Double] <- withSystemRandom . asGenST $ \gen -> replicateM 10000 ((method d) gen)
+    -- freqency: round, sort, group and link value and length of group
+    let hist::[(Double,[Int])] = map (\xs->(head xs,[length xs])) $ group $ sort $ map (roundTo 2) vals
+    -- generate bar chart
+    toFile def (show d ++ ".png") $ do
+        layout_title .= show d ++ " Distribution"
+        plot $ fmap plotBars (bars ["Frequency"] hist)
+    where
+        method Uniform = uniform
+        method Normal = standard
