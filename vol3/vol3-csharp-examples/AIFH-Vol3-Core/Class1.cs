0d9543c8db27f0bfa565270422e051a88973c27d
Jeff Heaton <jeff@jeffheaton.com>
2015-11-21 10:10:22 -0600

    vol 3: created C# project for examples... more to come

---
+++ vol3/vol3-csharp-examples/AIFH-Vol3-Core/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AIFH_Vol3_Core
+{
+    public class Class1
+    {
+    }
+}
